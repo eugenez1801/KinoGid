c7f666c2fb8b9bcf4eec95be742545bfdee156b1
Eugene <zhenyazl1801@mail.ru>
2025-03-21 14:34:59 +0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kinogid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.kinogid"
---
+++ app/src/main/java/com/example/kinogid/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kinogid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_auth)
+    }
+}
