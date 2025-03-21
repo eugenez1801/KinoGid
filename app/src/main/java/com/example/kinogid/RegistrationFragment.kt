07bc475f020f0d0010f1c5e6d5b7e0c8e6084544
Eugene <zhenyazl1801@mail.ru>
2025-03-21 20:11:11 +0300

    third commit

--- app/src/main/java/com/example/kinogid/RegistrationFragment.kt
+++ app/src/main/java/com/example/kinogid/RegistrationFragment.kt
@@ -7,19 +7,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class AuthorizationFragment: Fragment() {
+class RegistrationFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_authorization, container, false)
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+        val enterText = view.findViewById<TextView>(R.id.enterText)
 
-        val registerText = view.findViewById<TextView>(R.id.registrationText)
-        registerText.setOnClickListener{
-            val fragmentRegistration = RegistrationFragment.newInstance()
+        enterText.setOnClickListener{
+            val authorizationFragment = AuthorizationFragment.newInstance()
             val transaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragmentRegistration)
+            transaction.replace(R.id.fragment_container, authorizationFragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
@@ -27,8 +27,8 @@ class AuthorizationFragment: Fragment() {
     }
 
     companion object {
-        fun newInstance(): AuthorizationFragment {
-            return AuthorizationFragment()
+        fun newInstance(): RegistrationFragment {
+            return RegistrationFragment()
         }
     }
 }
